d861c2d9d1639a8b16568d77d5d68953022e05da
Rahmathidayat4299 <hrahmat950@gmail.com>
2023-02-26 19:47:42 +0700

    webpack error

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime';
+import './script/time/time.js';
+import './styles/style.css';
+import './script/component/app-bar.js';
+import main from './script/view/main.js';
+ 
+document.addEventListener('DOMContentLoaded', main);
